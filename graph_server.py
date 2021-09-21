46cde9dd67fe26c6ac31b25bbdd10da9306f0d5e
Lucas Oliveira <lucas.o@live.com>
2021-09-21 18:18:42 -0300

    update

--- graph_server.py
+++ graph_server.py
@@ -67,6 +67,17 @@ def get_theorem(theorem):
         else:
             step["log_complexity"] = 0
 
+    #Generate norm complexities
+    final_complexity = theorem_obj["complexity"]
+    for step in theorem_obj["steps"]:
+        step["norm_complexity"] = round(step["step_complexity"] / final_complexity, 5)
+
+    #Insert lemma complexities
+    for step in theorem_obj["steps"]:
+        if tdb[step["theorem"]]:
+            step["lemma_complexity"] = tdb[step["theorem"]]["complexity"]
+            step["lemma_log_complexity"] = round(math.log(step["lemma_complexity"]), 5)
+
     return jsonify(theorem_obj)
 
 def get_step_complexity(step, hyps_dict, steps_dict):
@@ -84,9 +95,11 @@ def get_step_complexity(step, hyps_dict, steps_dict):
                 _step_complexity += get_step_complexity(child_step, hyps_dict, steps_dict)
         
             step["step_complexity"] =  _step_complexity
+            step["lemma_complexity"] = step_theorem["complexity"]
 
         else:
             step["step_complexity"] = 0
+            step["lemma_complexity"] = 0
 
     return step["step_complexity"]
 
@@ -98,6 +111,15 @@ def get_theorem_graph(theorem):
     theorem_log_completixy = math.log(theorem_complexity)
 
     return render_template('theorem.html', theorem=theorem, complexity=theorem_complexity, log_complexity=theorem_log_completixy)
+
+
+@app.route("/linear/<theorem>")
+def get_theorem_linear(theorem):
+
+    theorem_complexity = tdb[theorem]["complexity"]
+    theorem_log_completixy = math.log(theorem_complexity)
+
+    return render_template('linear.html', theorem=theorem, complexity=theorem_complexity, log_complexity=theorem_log_completixy)
 
 
 # def parse_theorem(htmltext):
